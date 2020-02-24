a288a6807fdacda15cd9708b31ac3dd5b67e54ed
Thomas Long <thomas@Thomass-MacBook-Pro.local>
2020-02-24 10:56:08 -0600

    Added yellow goby & its details button

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -49,6 +49,14 @@ const initializeDetailButtonEvents = () => {
             theDialog.showModal()
         }
     )
+     // Show Jeff's details when the button is clicked
+     document.querySelector("#button--jeff").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--jeff")
+            theDialog.showModal()
+        }
+    )
 }
 
 export default initializeDetailButtonEvents
